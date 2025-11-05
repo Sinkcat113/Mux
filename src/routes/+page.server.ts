1f907f1742b0b6afc1aac3d504a9240e0e72e269
Sinkcat <132151681+Sinkcat113@users.noreply.github.com>
2025-11-05 16:11:04 -0600

    Add your own collection

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,9 +1,9 @@
-import { ADDRESS, API_KEY } from "$env/static/private";
+import { ADDRESS, API_KEY, COLLECTION } from "$env/static/private";
 import type { AlbumList, ArtistList } from "$lib/types";
 
 
 export const load = async () => {
-    const resp = await fetch(`${ADDRESS}/Items?parentId=6b0bcc893f2368c586f3d225262c8ac6&collectionType=music&api_key=${API_KEY}`);
+    const resp = await fetch(`${ADDRESS}/Items?parentId=${COLLECTION}&collectionType=music&api_key=${API_KEY}`);
     const data = await resp.json()
 
     const artistResp = await fetch(`${ADDRESS}/Artists?api_key=${API_KEY}`);
