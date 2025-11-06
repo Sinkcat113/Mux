2c9417f7600d939a667a0a6baaa915f3369e879a
Sinkcat <132151681+Sinkcat113@users.noreply.github.com>
2025-11-05 18:46:15 -0600

    Lol did it again

--- src/routes/artist/[id]/+page.server.ts
+++ src/routes/artist/[id]/+page.server.ts
@@ -1,4 +1,4 @@
-import { ADDRESS, API_KEY, USER_ID } from '$env/static/private';
+import { ADDRESS, API_KEY, USER_ID, COLLECTION} from '$env/static/private';
 import type { Album, Artist, ArtistList, TrackList } from '$lib/types.js';
 
 
@@ -6,7 +6,7 @@ export const load = async ({ params }) => {
     const resp = await fetch(`${ADDRESS}/Users/${USER_ID}/Items/${params.id}?api_key=${API_KEY}`)
     const data = await resp.json()
 
-    const tracksResp = await fetch(`${ADDRESS}/Items?ParentId=f589bad31f6609e91f1acf4b36eb9a3c&ArtistIds=${params.id}&IncludeItemTypes=MusicAlbum&Recursive=true&api_key=${API_KEY}`)
+    const tracksResp = await fetch(`${ADDRESS}/Items?ParentId=${COLLECTION}&ArtistIds=${params.id}&IncludeItemTypes=MusicAlbum&Recursive=true&api_key=${API_KEY}`)
     const tracks = await tracksResp.json()
 
     const artistResp = await fetch(`${ADDRESS}/Artists/${params.id}/Similar?api_key=${API_KEY}`);
